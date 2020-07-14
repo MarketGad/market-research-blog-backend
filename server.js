a19525ec79436a142690175f94cb554369ac08af
Ajitesh13 <aji.yash13@gmail.com>
2020-07-15 01:24:30 +0530

    cors configured

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ mongoose.connect(mongodburl, {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://kokopuri.netlify.app/'
+}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
