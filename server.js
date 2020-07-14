e7a46b28d56a2929d3d537d3fb455cd9e0d3793a
Ajitesh13 <aji.yash13@gmail.com>
2020-07-15 00:50:59 +0530

    cors allowed

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.get('*', (req, res) => {
