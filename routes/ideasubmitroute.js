21a45ce8dc9075113aea937bfef3eaef66117cce
Ajitesh13 <aji.yash13@gmail.com>
2020-07-19 12:00:48 +0530

    condition on ideasubmitroute updated

--- routes/ideasubmitroute.js
+++ routes/ideasubmitroute.js
@@ -14,38 +14,34 @@ router.put('/ideasubmit', async (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            if (user.ideagiven == false) {
-                if (user) {
-                    user.phone = req.body.phone;
-                    user.ideagiven = ideares;
-                    user.idea =  ideaget;
-                    user.friend1 = req.body.f1email;
-                    user.friend2 = req.body.f2email;
-                    user.sector = req.body.sector;
-                    user.name = user.name;
-                    user.email = user.email;
-                    user.picture = user.picture;
-                    const updateduser = await user.save();
-                    res.send({
-                        _id: updateduser.id,
-                        name: updateduser.name,
-                        email: updateduser.email,
-                        picture: updateduser.picture,
-                        phone: updateduser.picture,
-                        ideagiven: updateduser.ideagiven,
-                        idea: updateduser.idea,
-                        friend1: updateduser.friend1,
-                        friend2: updateduser.friend2,
-                        friend3: updateduser.friend3,
-                        sector: updateduser.sector
-                    });
-                    res.status(200).send({msg: 'Your Idea is Successfully Submitted, our team will contact your Soon!' })
-                    console.log(user.email);
-                } else {
-                    res.status(404).send({msg: 'Please Log In'});
-                }
+            if (user) {
+                user.phone = req.body.phone;
+                user.ideagiven = ideares;
+                user.idea =  ideaget;
+                user.friend1 = req.body.f1email;
+                user.friend2 = req.body.f2email;
+                user.sector = req.body.sector;
+                user.name = user.name;
+                user.email = user.email;
+                user.picture = user.picture;
+                const updateduser = await user.save();
+                res.send({
+                    _id: updateduser.id,
+                    name: updateduser.name,
+                    email: updateduser.email,
+                    picture: updateduser.picture,
+                    phone: updateduser.picture,
+                    ideagiven: updateduser.ideagiven,
+                    idea: updateduser.idea,
+                    friend1: updateduser.friend1,
+                    friend2: updateduser.friend2,
+                    friend3: updateduser.friend3,
+                    sector: updateduser.sector
+                });
+                res.status(200).send({msg: 'Your Idea is Successfully Submitted, our team will contact your Soon!' })
+                console.log(user.email);
             } else {
-                res.status(400).send({msg: 'Idea Already Submitted!'});
+                res.status(404).send({msg: 'Please Log In'});
             }
         }
     });
