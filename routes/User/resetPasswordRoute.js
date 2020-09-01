cff3ebf69b6012d46f01c7d36e59e4a1316c838a
ahtrahdis7 <sidharthabiki@gmail.com>
2020-09-01 23:20:06 +0530

    added reset password func

--- routes/User/resetPasswordRoute.js
+++ routes/User/resetPasswordRoute.js
@@ -3,19 +3,27 @@ const bodyParser = require('body-parser');
 const ResetPasswordRouter = express.Router();
 
 var authenticate = require('../../authenticate');
+const User = require('../../Models/UserNewModel');
 
 
 
 ResetPasswordRouter.use(bodyParser.json());
 ResetPasswordRouter.route('/')
     .post(authenticate.verifyUser, (req, res, next) => {
+        User.findOne({email: req.body.email}, (err, user) => {
+            if(user){
+                user.changePassword(req.body.oldpassword, req.body.newpassword, (err) => {
+                    res.statusCode = 403;
+                    res.end('Please Enter Correct Old Password');
+                })
+            } else {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({err: "User Not Found"});
+                return;
+            }
+        })
         
-        ProductDetails.create(req.body)
-        .then((profile) => {
-            console.log('Profile Created ', profile);
-            res.json(profile);
-        }, (err) => next(err))
-        .catch((err) => next(err));
     });
 
 
