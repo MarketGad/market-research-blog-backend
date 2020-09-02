e58ea9e0a5863fc317eaf38d871f92221ea820d6
Saidatta Sahu <saidatta.sahu02@gmail.com>
2020-09-03 00:17:28 +0530

    user bug fix

--- Models/UserNewModel.js
+++ Models/UserNewModel.js
@@ -1,40 +1,41 @@
 const mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-    },
-    firstname: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    lastname:{
-        type: String,
-        trim: true,
-        required: true,
-    },
-    picture: {
-        type: String,
-        required: false
-    },
-    phone: {
-        type: Number,
-        default: " ",
-        required: false,
-    },
-    isEmailVerified: {
-        type: Boolean,
-        default: false,
-        required: false,
-    },
+const UserSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true
+		},
+		firstname: {
+			type: String,
+			trim: true,
+			required: true
+		},
+		lastname: {
+			type: String,
+			trim: true,
+			required: true
+		},
+		picture: {
+			type: String,
+			required: false
+		},
+		phone: {
+			type: Number,
+			required: false
+		},
+		isEmailVerified: {
+			type: Boolean,
+			default: false,
+			required: false
+		}
+	},
+	{ timestamps: true }
+);
 
-}, { timestamps: true });
-
-UserSchema.plugin(passportLocalMongoose, { usernameField : 'email'});
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
 module.exports = mongoose.model('NewUser', UserSchema);
