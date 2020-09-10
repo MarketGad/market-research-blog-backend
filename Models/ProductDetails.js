a99c148e6f34843fc676b87e48e86137adb2cff2
Saidatta Sahu <saidatta.sahu02@gmail.com>
2020-09-10 15:35:49 +0530

    default null removed

--- Models/ProductDetails.js
+++ Models/ProductDetails.js
@@ -1,103 +1,98 @@
 const mongoose = require('mongoose');
 
-const CommentSchema = new mongoose.Schema({
-    comment: {
-        type: String,
-        required: false
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'NewUser'
-    },
-}, {
-    timestamps: true
-});
+const CommentSchema = new mongoose.Schema(
+	{
+		comment: {
+			type: String,
+			required: false
+		},
+		author: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'NewUser'
+		}
+	},
+	{
+		timestamps: true
+	}
+);
 
 const UpvotesList = new mongoose.Schema({
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'NewUser',
-        unique: true,
-    }
-})
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'NewUser',
+		unique: true
+	}
+});
 
+const ProductDetails = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'NewUser'
+		},
+		logo: {
+			type: String,
+			required: false
+		},
+		name: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true
+		},
+		websiteLink: {
+			type: String,
+			required: false
+		},
+		playStoreLink: {
+			type: String,
+			required: false
+		},
+		appStoreLink: {
+			type: String,
+			required: false
+		},
+		briefDescription: {
+			type: String,
+			required: false
+		},
+		detailedDescription: {
+			type: String,
+			required: false
+		},
+		upvotes: {
+			type: Number,
+			default: 0,
+			required: false
+		},
+		upvotesList: [ UpvotesList ],
 
-const ProductDetails = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'NewUser',
-    },
-    logo: {
-        type: String,
-        default: "",
-        required: false
-    },
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-    },
-    websiteLink:{
-        type: String,
-        default: "",
-        required: false
-    },
-    playStoreLink: {
-        type: String,
-        default: "",
-        required: false,
-    },
-    appStoreLink: {
-        type: String,
-        default: "",
-        required: false,
-    },
-    briefDescription: {
-        type: String,
-        default: "",
-        required: false
-    },
-    detailedDescription: {
-        type: String,
-        default: "",
-        required: false
-    },
-    upvotes: {
-        type: Number,
-        default: 0,
-        required: false,
-    },
-    upvotesList: [UpvotesList],
-
-    // reputation point = 4 * no of comments + 1 * upvotes
-    reputationPoint: {
-        type: Number,
-        default: 0,
-        required: false,
-    },
-    pointOfContact:{
-        type: String,
-        default: "",
-        required: false
-    },
-    emailId:{
-        type: String,
-        trim: true,
-        required: true,
-        unique: true
-    },
-    // rating: {
-    //     type: Number,
-    //     min: 1,
-    //     max: 5,
-    //     default: 1,
-    //     required: false,
-    // },
-    comments: [CommentSchema]
-
-
-}, { timestamps: true });
-
+		// reputation point = 4 * no of comments + 1 * upvotes
+		reputationPoint: {
+			type: Number,
+			default: 0,
+			required: false
+		},
+		pointOfContact: {
+			type: String,
+			required: false
+		},
+		emailId: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true
+		},
+		// rating: {
+		//     type: Number,
+		//     min: 1,
+		//     max: 5,
+		//     default: 1,
+		//     required: false,
+		// },
+		comments: [ CommentSchema ]
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('ProductDetails', ProductDetails);
