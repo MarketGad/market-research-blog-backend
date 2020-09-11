2334a5cdcbc36dd00bd94b57ce2f40e049e53ac8
Saidatta Sahu <saidatta.sahu02@gmail.com>
2020-09-11 22:30:36 +0530

    profilePic added in jobprofile model

--- Models/JobProfile.js
+++ Models/JobProfile.js
@@ -1,70 +1,71 @@
 const mongoose = require('mongoose');
 
-
 // work on using mongoose.populate to populate the user details here.
-const JobProfile = new mongoose.Schema({
-    
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        unique: true,
-        ref: 'NewUser'
-    },
-    rating: {
-        type: Number,
-        required: false,
-    },
-    isHired: {
-        type: Number,
-        required: false
-    },
-    connections: {
-        type: [String],
-        required: false,
-    },
-    skills: {
-        type: [String],
-        required: false,
-    },
-    location: {
-        type: [String],
-        required: false,
-    },
-    experience:{
-        type: [String],
-        required: false,
-    },
-    qualification:{
-        type: [String],
-        required: false,
-    },
-    passionateAbout:{
-        type: [String],
-        required: false,
-    },
-    portfolioLink:{
-        type: String,
-        required: false,
-    },
-    linkedIn:{
-        type: String,
-        required: false,
-    },
-    serviceName:{
-        type: String,
-        required: false,
-    },
-    breifDetails:{
-        type: String,
-        required: false,
-    },
-    offeringPrice:{
-        type: Number,
-        required: false,
-    }
-
-
-
-}, { timestamps: true });
-
+const JobProfile = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			unique: true,
+			ref: 'NewUser'
+		},
+		rating: {
+			type: Number,
+			required: false
+		},
+		isHired: {
+			type: Number,
+			required: false
+		},
+		connections: {
+			type: [ String ],
+			required: false
+		},
+		skills: {
+			type: [ String ],
+			required: false
+		},
+		location: {
+			type: [ String ],
+			required: false
+		},
+		experience: {
+			type: [ String ],
+			required: false
+		},
+		qualification: {
+			type: [ String ],
+			required: false
+		},
+		passionateAbout: {
+			type: [ String ],
+			required: false
+		},
+		portfolioLink: {
+			type: String,
+			required: false
+		},
+		linkedIn: {
+			type: String,
+			required: false
+		},
+		serviceName: {
+			type: String,
+			required: false
+		},
+		breifDetails: {
+			type: String,
+			required: false
+		},
+		offeringPrice: {
+			type: Number,
+			required: false
+		},
+		profilePic: {
+			type: String,
+			required: false
+		}
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('JobProfile', JobProfile);
