49af279bd4bca19c89db234d85563687a8161e0c
ahtrahdis7 <sidharthabiki@gmail.com>
2020-10-13 20:41:05 +0530

    added comments and upvote routes

--- routes/DisrupterClub/PostsRouter.js
+++ routes/DisrupterClub/PostsRouter.js
@@ -4,6 +4,7 @@ var passport = require('passport');
 const User = require('../../Models/UserNewModel');
 const ClubPosts = require('../../Models/ClubPosts');
 var authenticate = require('../../authenticate');
+const productDetailsRouter = require('../productsDetails/productDetailsRouter');
 
 
 const PostsRouter = express.Router();
@@ -12,6 +13,7 @@ PostsRouter.route('/')
 .get((req, res, next) => {
     ClubPosts.find({})
     .populate('user')
+    .populate('comments.author')
     .then((posts) => {
         // console.log(posts);
         if(posts.length > 0){
@@ -20,6 +22,13 @@ PostsRouter.route('/')
             res.json({
                 status: "success",
                 posts
+            });
+        } else {
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+                status: "success",
+                posts : "No Posts Yet"
             });
         }
     }, (err) => next(err))
@@ -37,6 +46,85 @@ PostsRouter.route('/')
         });
         post.upvotes.push(post._id);
         post.save;
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('operation not supported yet');
+})
+.delete((req, res, next) => {
+    res.statusCode = 403;
+    res.end('operation not supported yet');
+})
+
+
+PostsRouter.route('/:postId/comments')
+.get((req, res, next) => {
+    ClubPosts.findById(req.params.postId)
+    .populate('comments.author')
+    .populate('user') 
+    .then((post) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            status: "success",
+            post
+        });
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+    ClubPosts.findById(req.params.postId)
+    .then((post) => {
+        if(post != null){
+
+            post.comments.push(req.body);
+            post.save();
+
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+                status: "success",
+                post: post
+            });
+
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('operation not supported yet');
+})
+.delete((req, res, next) => {
+    res.statusCode = 403;
+    res.end('operation not supported yet');
+})
+
+PostsRouter.route('/:postId/upvote')
+.get((req, res, next) => {
+    res.statusCode = 403;
+    res.end('GET operation not supported yet');
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+    ClubPosts.findById(req.params.postId)
+    .then((post) => {
+        if(post != null){
+
+            if(post.upvotes.indexOf(req.user._id) >= 0){
+                res.statusCode = 404
+                res.setHeader('Content-Type', 'application/json')
+                res.json({success: false, message: "already upvoted"})
+            } else {
+                post.upvotes.push(req.user._id);
+                post.save();
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json({success: true, message: "upvote added"}) 
+            }
+
+        }
     }, (err) => next(err))
     .catch((err) => next(err))
 })
